520b59149f021405735f96259b8b9125292f91f4
Wei-Lun Ma <weilunma@gmail.com>
2020-06-07 18:39:00 +0200

    Updates

---
+++ precache-manifest.ce7b2204a1223538b73623d7be68536c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d98d8bf8a2c9953051284e638c1d58",
+    "url": "/react-shopping-cart/index.html"
+  },
+  {
+    "revision": "da8c129b37b3b3d29c5d",
+    "url": "/react-shopping-cart/static/css/main.3e95df8f.chunk.css"
+  },
+  {
+    "revision": "3f0de0c5fd5aeb485b33",
+    "url": "/react-shopping-cart/static/js/2.ae0a4813.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/react-shopping-cart/static/js/2.ae0a4813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da8c129b37b3b3d29c5d",
+    "url": "/react-shopping-cart/static/js/main.bfb6dfe0.chunk.js"
+  },
+  {
+    "revision": "829297b007ca06de3fed",
+    "url": "/react-shopping-cart/static/js/runtime-main.8f3f9671.js"
+  }
+]);
